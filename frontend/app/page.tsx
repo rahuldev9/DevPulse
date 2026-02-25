a952f2b8dfbe755679b69d32d6b5eb718073c1f1
Rahul <webdeveloperaim@gmail.com>
2026-02-25 20:11:39 +0530

    changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,13 @@
 // app/page.tsx
 
+export const dynamic = "force-dynamic"; // ✅ ADD THIS
+
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import HomeClient from "@/components/Homeclient";
 
 export default async function Home() {
   const cookieStore = await cookies();
-  const token = cookieStore.get("token")?.value; // ✅ FIX
-
-  // if (token) {
-  //   redirect("/dashboard");
-  // }
+  const token = cookieStore.get("token")?.value;
 
   return <HomeClient token={token} />;
 }
